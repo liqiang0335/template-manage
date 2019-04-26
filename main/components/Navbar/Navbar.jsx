f6be6f873f1f46f6ba3fb4459047091baec2ff72
liqiang <liqiang0335@qq.com>
2019-04-26 10:20:44 +0800

    add

---
+++ main/components/Navbar/Navbar.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import styles from "./Navbar.scss";
+import hub, { OPEN_URL } from "../../script/hub";
+
+const data = [
+  {
+    id: "a1",
+    name: "系统管理",
+    children: [
+      {
+        id: "a3",
+        name: "角色管理",
+        url: "http://www.qq.com"
+      },
+      {
+        id: "a4",
+        name: "菜单管理",
+        url: "http://www.sina.com"
+      },
+      {
+        id: "a5",
+        name: "部门管理",
+        url: "http://www.ifeng.com"
+      }
+    ]
+  }
+];
+
+export default function Navbar() {
+  const [update, setUpdate] = useState(0);
+
+  const showChildren = item => {
+    item.show = !item.show;
+    setUpdate(Date.now());
+  };
+
+  const onSelect = sub => {
+    hub.emit(OPEN_URL, sub.url);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}> 后台管理 </div>{" "}
+      {data.map(item => (
+        <div key={item.id} className={styles.group}>
+          <div className={styles.name1} onClick={showChildren.bind(null, item)}>
+            <span className={styles.icon1}>
+              <i className="icon-cog2" />
+            </span>{" "}
+            {item.name}{" "}
+          </div>{" "}
+          <div
+            className={styles.children}
+            style={{
+              display: item.show ? "none" : "flex"
+            }}
+          >
+            {" "}
+            {(item.children || []).map(sub => (
+              <div
+                key={sub.id}
+                className={styles.name2}
+                onClick={onSelect.bind(null, sub)}
+              >
+                {" "}
+                {sub.name}{" "}
+                <span className={styles.icon2}>
+                  <i className="icon-navigate_next2" />
+                </span>{" "}
+              </div>
+            ))}{" "}
+          </div>{" "}
+        </div>
+      ))}{" "}
+    </div>
+  );
+}
