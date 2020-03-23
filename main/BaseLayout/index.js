567f2e15ec8ba479db40d7cdceaaad87e94d6c86
liqiang <liqiang0335@qq.com>
2020-03-23 17:13:28 +0800

    add ant config

--- main/BaseLayout/index.js
+++ main/BaseLayout/index.js
@@ -4,19 +4,23 @@ import styles from "./index.scss";
 import Header from "./Header/Header";
 import Menu from "./Menu/Menu";
 import { HashRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/es/locale/zh_CN";
 /**
  * 顶部
  */
 export default function Common({ children }) {
   return (
-    <HashRouter>
-      <div className={styles.container}>
-        <Menu />
-        <div className={styles.detail}>
-          <Header />
-          <div className={styles.contents}>{children}</div>
+    <ConfigProvider locale={zhCN}>
+      <HashRouter>
+        <div className={styles.container}>
+          <Menu />
+          <div className={styles.detail}>
+            <Header />
+            <div className={styles.contents}>{children}</div>
+          </div>
         </div>
-      </div>
-    </HashRouter>
+      </HashRouter>
+    </ConfigProvider>
   );
 }
